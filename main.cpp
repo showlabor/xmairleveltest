c79b1bfa82b509726a6ef8a885b0df762f0b873d
Felix Homann <linuxaudio@showlabor.de>
2017-05-22 23:00:21 +0200

    Added a test for different rounding schemes of float values.

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@
  *
  */
 
+#include <cmath>
 #include <iostream>
 #include <vector>
 
@@ -39,14 +40,37 @@ int main(int argc, char* argv[])
     return -1;
   }
 
-  uint num_steps = 1024;
+  uint num_steps = 1024*4;
   tester.run_tests(mixer, num_steps, true);
+
+  std::cout << "\nThe expected result currently is that we get two dB mismatches for index 765 and 769 respectively."
+  	    << "\nThe desktop apps seem to give the same dB values for those levels.\n" << std::endl;
 
   // // Count distinct dB Strings.
   tester.count_node_db(mixer);
+  std::cout << "\nExpected number of distinct values is 658.\n" << std::endl;
 
-  std::cout << "\nThe expected result currently is that we get two dB mismatches for index 765 and 769 respectively."
-	    << "\nThe desktop apps seem to give the same dB values for those levels." << std::endl;
+  // Check Patrick-Gilles Maillot's rounding formula
+  // as given on page 110 of his X32 OSC documentation.
+  std::cout << "The Xrm32Level's mapping/rounding of float values to the actual console values"
+	    << " should have proved correct above."
+	    << "\nNow check Patrick-Gilles Maillot's rounding via roundf().\n" << std::endl;
+  Xrm32::Level<1024> level;
+  int count = 0;
+  for (int i = 0; i < num_steps; ++i) {
+    float flevel = i * 1.f /(num_steps - 1);
+    level.setFloat(flevel);
+    float rounded = roundf(flevel * 1023) / 1023; // rounding according to p.110
+    float actual_value = level.getFloat();
+    bool match = rounded == actual_value;
+    if (!match) {
+      ++count;
+    }
+    // std::cout << "flevel: " << flevel << "   Actual: " << actual_value << "   roundf: "
+    // 	      << rounded << "   match: " << match << std::endl;
+  }
+
+  std::cout << "roundf() rounding didn't match the correct values " << count  << " times!" << std::endl;
 
   return 0;
 }
